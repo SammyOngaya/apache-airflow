8da5ecd8788df4632240c9d20eed9c505e26566e
soongaya <sammiongaya@gmail.com>
2021-01-16 10:42:04 +0300

    Added airflow script for executing notebooks and python scripts with Bash Command

--- dags/airflow-execute-data-processing-notebook-and-python-scripts.py
+++ dags/airflow-execute-data-processing-notebook-and-python-scripts.py
@@ -5,10 +5,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 
 
-# step 2 -- define your python functions
-
-
-# step 3 -- define the defaults arguments
+# step 2 -- define the defaults arguments
 default_args={
     'owner':'sam',
     'depends_on_past':False,
@@ -16,12 +13,12 @@ default_args={
     'retries':3
     }
 
-# step 4 -- define your DAG
+# step 3 -- define your DAG
 # run every 5 mins (schedule_interval='*/5 * * * *') 
 dag = DAG('Tweet_Analysis_DAG-1',default_args=default_args, description='Sample DAG to execute Python Script', schedule_interval='*/5 * * * *', 
 		catchup=False)
 
-# step 5 -- instantiate your tasks
+# step 4 -- instantiate your tasks
 dummy_operator = DummyOperator(task_id='dummy_task', dag=dag)
 tweets_python_bash_operator = BashOperator(task_id='tweet_python_analysis_task',
     bash_command='python3 /mnt/c/Users/soongaya/Documents/datasets/Twitter_Data_Extraction.py',
@@ -33,6 +30,6 @@ twitter_bash_notebook_operator = BashOperator(task_id='tweet_analysis_task',
     dag=dag)
 # )/mnt/c/Users/soongaya/Documents/Data Science/Big Data/usecases/Natural-Language-Processing/Social Analytics/Twitter Data Extraction.ipynb'
 
-# step 6 -- set task dependencies
+# step 5 -- set task dependencies
 dummy_operator >> twitter_bash_notebook_operator
 # dummy_operator >> tweets_python_bash_operator
