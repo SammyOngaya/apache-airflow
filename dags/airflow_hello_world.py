b5bf9d0519b83f648bd81baf4cd94bde9a6e1376
Sammy Ongaya <sammiongaya@gmail.com>
2021-11-15 12:45:05 +0300

    Updated the code for data2ml

--- dags/airflow_hello_world.py
+++ dags/airflow_hello_world.py
@@ -1,9 +1,9 @@
-# Step 1
+# Step 1 Import required libraries
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.dummy_operator import DummyOperator
 
-# Step 2
+# Step 2 Define default parameters
 
 default_args={
     'owner':'airflow',
@@ -12,15 +12,15 @@ default_args={
     'retries':3
 }
 
-# Step 3
+# Step 3 Create DAG Object
 dag=DAG(dag_id='DAG-1', default_args=default_args, catchup=False, schedule_interval='* * * * *')
 
-# Step 4
+# Step 4 Add Tasks to the DAG
 
 start=DummyOperator(task_id='start', dag=dag)
 end=DummyOperator(task_id='end',dag=dag)
 
-# Step 5
+# Step 5 Define Dependencies
 start>>end
 
 
